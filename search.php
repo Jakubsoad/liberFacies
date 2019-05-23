5a40fe4d84f4508a6c04e4dd736fa94803314de7
Jakub <jakub.soad@gmail.com>
2019-05-23 12:42:31 +0200

    21.4.5

--- search.php
+++ search.php
@@ -11,12 +11,23 @@ require_once 'dbConn.php';
 
 $friend=filter_input(INPUT_POST, 'friend');
 $friend=mysqli_real_escape_string($conn, $friend);
+$query = "SELECT login from User where email like '%$friend%' or login like '%$friend%'";
+$querySearch = mysqli_query($conn, $query);
 
-$querySearch = mysqli_query($conn, "SELECT id from User where email='$friend' or login='$friend'");
+
 $numRows = mysqli_num_rows($querySearch);
 if ($numRows==0)
 {
     $_SESSION['friend']="Your friend doesn't exist :(";
     header("Location: main.php");
     exit();
+}
+$querySearch = mysqli_fetch_all($querySearch);
+$counter = 1;
+foreach ($querySearch as $q)
+{
+    foreach ($q as $a) {
+        echo $counter .". ". $a."<br>";
+        $counter++;
+    }
 }
